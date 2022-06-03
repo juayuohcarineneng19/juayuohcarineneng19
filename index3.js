575d93f19368e35cdb7b5ea0d4bbd668c4f27461
jua <juayuoh>
2022-06-03 23:27:20 +0200

    initial commit

---
+++ index3.js
@@ -0,0 +1,9 @@
+function myFuncSnack() {
+    
+    var x = document.getElementById("snackbar")
+  
+    
+    x.className = "show";
+  
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+  }
