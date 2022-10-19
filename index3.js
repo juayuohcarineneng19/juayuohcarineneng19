9f6963cfff9868fafc7391b7c3204a651fe53af0
https://github.com/juayuohcarineneng19/ <juayuoh@gmail.com>
2022-10-19 15:43:17 -0700

    added special readme

--- index3.js
+++
@@ -1,9 +0,0 @@
-function myFuncSnack() {
-    
-    var x = document.getElementById("snackbar")
-  
-    
-    x.className = "show";
-  
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-  }
